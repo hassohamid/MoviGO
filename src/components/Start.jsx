e13996ab87a0bae1183d2de77b90a87d4c90f42b
Mark Taratynov <mark.taratynov@gmail.com>
2024-11-27 16:44:16 +0100

    color

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,7 +1,9 @@
 export default function Start() {
   return (
     <section className="w-[400px] flex flex-col gap-5">
-      <article className="text-center">Welcome to ... </article>
+      <article className="text-center text-white text-5xl font-extrabold drop-shadow-md">
+        Welcome to ...{" "}
+      </article>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat nam ea
         dolore, quidem obcaecati repellat ipsa aliquam incidunt hic dolorum
