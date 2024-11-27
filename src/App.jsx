687d8c9a04a15eddc634dfd5c7ed47babd3ab43f
hassohamid <hassohamid@hotmail.com>
2024-11-27 16:43:46 +0100

    animatedbg

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Home from "./pages/Home";
 import Login from "./components/Login";
 import Start from "./components/Start";
 import Favorites from "./pages/Favorites";
+import { AnimatedBackground } from "animated-backgrounds";
 
 function App() {
   return (
     <div className="app-container">
+      <AnimatedBackground animationName="auroraBorealis" blendMode="lighten" />
       <Start />
     </div>
   );
